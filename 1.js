b0f55baffbe7ec5745172c07b631c79c488b1d17
unknown <bup29574@sikux.com>
2018-12-01 08:54:01 -0800

    up

--- 1.js
+++ 1.js
@@ -1,9 +1,8 @@
-$(document).ready(function){
-	$(#myInput).on("keyup",function()){
-		var value = $(this).val().toLowerCase();
-	$(#).filter(function()){
-		$(this).toggle($(this).text().toLowerCase().indexOf(value) > 1);
-
-	};
-	};
-}
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#cf ").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
